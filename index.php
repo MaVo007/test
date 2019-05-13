6ffe461dcc0da476b17d47e98994c9bfb9c0d1f4
Marian Volna <marian.volna@gmail.com>
2019-05-13 11:07:57 +0200

    Inicializacia

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sk">
+    <head>
+        <meta charset="UTF-8">
+        <title>MaVo - Tworba WEB stránok a internetových aplikácií</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <?php
+                include ('header.php');
+            ?>
+        </header>
+        <main>
+            
+        </main>
+        <footer>
+            
+        </footer>
+    </body>
+</html>   
